f05ca487b7234f4841abc25b9bf07346a42c2660
Aryan <aryandtu1@gmail.com>
2020-11-22 01:24:32 +0530

    Adding apache camel dependencies

---
+++ src/main/java/com/dataIntegration/Skyscraper/MyBean.java
@@ -0,0 +1,10 @@
+package com.dataIntegration.Skyscraper;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyBean {
+
+  Integer id;
+  String name;
+}
--- src/main/java/com/dataIntegration/Skyscraper/SkyscraperApplication.java
+++ src/main/java/com/dataIntegration/Skyscraper/SkyscraperApplication.java
@@ -1,13 +1,30 @@
 package com.dataIntegration.Skyscraper;
 
+import org.apache.camel.component.servlet.CamelHttpTransportServlet;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.dataIntegration.Skyscraper")
 public class SkyscraperApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SkyscraperApplication.class, args);
-	}
+  @Value("${baeldung.api.path}")
+  String contextPath;
+
+  @Bean
+  ServletRegistrationBean servletRegistrationBean() {
+    ServletRegistrationBean servlet = new ServletRegistrationBean
+        (new CamelHttpTransportServlet(), contextPath + "/*");
+    servlet.setName("CamelServlet");
+    return servlet;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(SkyscraperApplication.class, args);
+  }
 
 }
