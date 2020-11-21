c2a2198d77a7a9c1c4ec02528423bd9bb84c7a80
Aryan <aryandtu1@gmail.com>
2020-11-22 00:37:24 +0530

    Initial Commit with start.spring.io

---
+++ src/main/java/com/dataIntegration/Skyscraper/SkyscraperApplication.java
@@ -0,0 +1,13 @@
+package com.dataIntegration.Skyscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyscraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyscraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dataIntegration/Skyscraper/SkyscraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataIntegration.Skyscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyscraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
